18a6d9cd6f131e5ba698078b2153c050d42d7991
katelandcastelin <kateland.cas@gmail.com>
2024-01-30 15:02:49 +0200

    feat/gallery-frames: added frame styles to galley page

---
+++ app/pages/gallery/artworks-data/artworks.js
@@ -0,0 +1,40 @@
+const ArtworkImages = [
+  {
+    id: '/artworks/anon.png',
+    title: 'anon',
+    artist: 'cardboard',
+    year: '2022',
+  },
+  {
+    id: '/images/artworks/almond-blossoms.jpeg',
+    title: 'Almond Blossoms',
+    artist: 'Vincent Van Gogh',
+    year: '1890',
+    corsets: [
+      {
+        image: '/images/artworks/almond-blossoms/1.jpeg',
+        description: '',
+      },
+      {
+        image: '/images/artworks/almond-blossoms/1-1.jpeg',
+        description: '',
+      },
+      {
+        image: '/images/artworks/almond-blossoms/1-2.jpeg',
+        description: '',
+      },
+      {
+        image: '/images/artworks/almond-blossoms/1-3.jpeg',
+        description: '',
+      },
+      {
+        image: '/images/artworks/almond-blossoms/2.jpg',
+        description: '',
+      },
+      {
+        image: '/images/artworks/almond-blossoms/3.jpg',
+        description: '',
+      },
+    ],
+  },
+]
--- app/pages/gallery/page.jsx
+++ app/pages/gallery/page.jsx
@@ -1,7 +1,38 @@
+'use client';
+import styles from './galleryPage.module.css';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  columns: 2 200px;
+  column-gap: auto;
+  width: 90%;
+  margin: 0 auto;
+  padding-top: 1rem;
+`;
+
+const Artwork = styled.img`
+  max-height: 100%;
+  max-width: 100%;
+  border: solid 2px;
+  border-bottom-color: #ffe;
+  border-left-color: #eed;
+  border-right-color: #eed;
+  border-top-color: #ccb;
+  position: inherit;
+  z-index: 1;
+
+  :hover {
+    cursor: pointer;
+  }
+`;
+
 export default function GalleryPage() {
+
   return (
-    <div>
-      Gallery
-    </div>
-  )
+    <Container>
+      <div className={styles.frame}>
+        <Artwork src='/artworks/anon.png' />
+      </div>
+    </Container>
+  );
 }
