83750902b96c2a6a20d205592e70a5cd064205aa
katelandcastelin <kateland.cas@gmail.com>
2024-03-13 08:21:09 +0200

    feat/home-page-banner: added animation to text

--- app/page.js
+++ app/page.js
@@ -10,12 +10,16 @@ const slideInAndOut = keyframes`
   55%, 95% {top: -100%; opacity: 0;}
 `;
 
+const fadeIn = keyframes`
+  0%, 8%, 100% { opacity: 0; }
+  13%, 95% { opacity: 1; }
+`;
+
 const BannerContainer = styled.div`
   display: flex;
   justify-content: center;
   height: 400px;
   width: 100%;
-  border-bottom: 1px solid #fff;
   background-color: #000;
   overflow: hidden;
   position: relative;
@@ -42,7 +46,7 @@ const ImageContainer = styled.div`
 const StyledImage = styled.img`
   width: 100%;
   height: 100%;
-  object-fit: cover;
+  object-fit: contain;
   z-index: -1;
 `;
 
@@ -50,6 +54,8 @@ const TextContainer = styled.div`
   color: #fff;
   z-index: 2;
   position: absolute;
+  animation: ${fadeIn} 12s infinite ease-in-out;
+  animation-delay: ${props => props.delay + 0.1}s;
 `;
 
 export default function Home() {
@@ -58,16 +64,16 @@ export default function Home() {
       <Header />
       <BannerContainer>
         <Banner delay={0}>
-          <TextContainer>
-            {/* <h1>Title for banner 1</h1> */}
+          <TextContainer delay={0}>
+            <h1>Title for banner 1</h1>
           </TextContainer>
           <ImageContainer>
             <StyledImage src="/art-display/1.jpeg" />
           </ImageContainer>
         </Banner>
         <Banner delay={6}>
-          <TextContainer>
-            {/* <h1>Title for banner 2</h1> */}
+          <TextContainer delay={6}>
+            <h1>Title for banner 2</h1>
           </TextContainer>
           <ImageContainer>
             <StyledImage src="/art-display/2.jpeg" />
