9fa78c02e44e68e1045e5a5c41b8e17d9a8ae5fe
katelandcastelin <kateland.cas@gmail.com>
2024-03-13 08:00:54 +0200

    feat/home-page-banner: changed styling and functionality to slide between banner images

--- app/page.js
+++ app/page.js
@@ -1,34 +1,13 @@
 'use client';
-import React, { useState, useEffect } from "react";
+import React from "react";
 import styled, { keyframes } from "styled-components";
 import styles from "./page.module.css";
 import Header from "./components/Header";
 
-const slideIn = keyframes`
-  from {
-    transform: translateY(-100%);
-  }
-  to {
-    transform: translateY(0);
-  }
-`;
-
-const slideOut = keyframes`
-  from {
-    transform: translateY(0);
-  }
-  to {
-    transform: translateY(100%);
-  }
-`;
-
-const fadeIn = keyframes`
-  from {
-    opacity: 0;
-  }
-  to {
-    opacity: 1;
-  }
+const slideInAndOut = keyframes`
+  0%, 100% {top: 100%; opacity: 0;}
+  5%, 50% {top: 0%; opacity: 1;}
+  55%, 95% {top: -100%; opacity: 0;}
 `;
 
 const BannerContainer = styled.div`
@@ -39,12 +18,20 @@ const BannerContainer = styled.div`
   border-bottom: 1px solid #fff;
   background-color: #000;
   overflow: hidden;
+  position: relative;
 `;
 
 const Banner = styled.div`
-  max-width: 1500px;
+  position: absolute;
+  top: 100%;
+  left: 0;
   width: 100%;
-  animation: ${(props) => (props.isEntering ? slideIn : slideOut)} 1s forwards;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  animation: ${slideInAndOut} 12s infinite ease-in-out;
+  animation-delay: ${props => props.delay}s;
 `;
 
 const ImageContainer = styled.div`
@@ -61,56 +48,30 @@ const StyledImage = styled.img`
 
 const TextContainer = styled.div`
   color: #fff;
-  z-index: 1;
+  z-index: 2;
   position: absolute;
-  opacity: 0;
-  animation: ${fadeIn} 1s forwards;
-  animation-delay: 1.5s; 
 `;
 
 export default function Home() {
-  const [activeBanner, setActiveBanner] = useState(1);
-  const [isEntering, setIsEntering] = useState(true);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setIsEntering(false);
-      setTimeout(() => {
-        setActiveBanner((prevActiveBanner) => (prevActiveBanner === 1 ? 2 : 1));
-        setIsEntering(true);
-      }, 1000);
-    }, 4000);
-
-    return () => clearInterval(interval);
-  }, []);
-
   return (
     <main className={styles.main}>
       <Header />
       <BannerContainer>
-        <Banner isEntering={activeBanner === 1 && isEntering} style={{ display: activeBanner === 1 ? 'flex' : 'none' }}>
-          {activeBanner === 1 && (
-            <div style={{ display: 'flex' }}>
-              <TextContainer>
-                <h1>Title for banner 1</h1>
-              </TextContainer>
-              <ImageContainer>
-                <StyledImage src="/art-display/1.jpeg" />
-              </ImageContainer>
-            </div>
-          )}
+        <Banner delay={0}>
+          <TextContainer>
+            {/* <h1>Title for banner 1</h1> */}
+          </TextContainer>
+          <ImageContainer>
+            <StyledImage src="/art-display/1.jpeg" />
+          </ImageContainer>
         </Banner>
-        <Banner isEntering={activeBanner === 2 && isEntering} style={{ display: activeBanner === 2 ? 'flex' : 'none' }}>
-          {activeBanner === 2 && (
-            <div style={{ display: 'flex' }}>
-              <TextContainer>
-                <h1>Title for banner 2</h1>
-              </TextContainer>
-              <ImageContainer>
-                <StyledImage src="/art-display/2.jpeg" />
-              </ImageContainer>
-            </div>
-          )}
+        <Banner delay={6}>
+          <TextContainer>
+            {/* <h1>Title for banner 2</h1> */}
+          </TextContainer>
+          <ImageContainer>
+            <StyledImage src="/art-display/2.jpeg" />
+          </ImageContainer>
         </Banner>
       </BannerContainer>
     </main>
