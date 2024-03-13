77c029fc1cdb3bfc1a98d409c2b9b348db4f753d
katelandcastelin <kateland.cas@gmail.com>
2024-03-13 09:27:56 +0200

    feat/home-page-banner: changed banner display time and added styling

--- app/page.js
+++ app/page.js
@@ -5,14 +5,27 @@ import styles from "./page.module.css";
 import Header from "./components/Header";
 
 const slideInAndOut = keyframes`
-  0%, 100% {top: 100%; opacity: 0;}
-  5%, 50% {top: 0%; opacity: 1;}
-  55%, 95% {top: -100%; opacity: 0;}
+  0%, 100% {
+    top: 100%;
+    opacity: 0;
+  }
+  4.16%, 50% {
+    top: 0%;
+    opacity: 1;
+  }
+  54.16%, 95.83% {
+    top: -100%;
+    opacity: 0;
+  }
 `;
 
 const fadeIn = keyframes`
-  0%, 8%, 100% { opacity: 0; }
-  13%, 95% { opacity: 1; }
+  0%, 8%, 100% {
+    opacity: 0;
+  }
+  13%, 95% {
+    opacity: 1;
+  }
 `;
 
 const BannerContainer = styled.div`
@@ -20,21 +33,22 @@ const BannerContainer = styled.div`
   justify-content: center;
   height: 400px;
   width: 100%;
-  background-color: #000;
   overflow: hidden;
   position: relative;
+  /* background-color: cornsilk; */
 `;
 
 const Banner = styled.div`
+  max-width: 1700px;
   position: absolute;
   top: 100%;
-  left: 0;
+  left: 50%;
+  transform: translateX(-50%);
   width: 100%;
   height: 100%;
   display: flex;
-  justify-content: center;
   align-items: center;
-  animation: ${slideInAndOut} 12s infinite ease-in-out;
+  animation: ${slideInAndOut} 24s infinite ease-in-out;
   animation-delay: ${props => props.delay}s;
 `;
 
@@ -46,15 +60,16 @@ const ImageContainer = styled.div`
 const StyledImage = styled.img`
   width: 100%;
   height: 100%;
-  object-fit: contain;
+  object-fit: cover;
   z-index: -1;
 `;
 
 const TextContainer = styled.div`
   color: #fff;
+  text-shadow: 2px 2px 8px #000;
   z-index: 2;
   position: absolute;
-  animation: ${fadeIn} 12s infinite ease-in-out;
+  animation: ${fadeIn} 24s infinite ease-in-out;
   animation-delay: ${props => props.delay + 0.1}s;
 `;
 
@@ -63,22 +78,24 @@ export default function Home() {
     <main className={styles.main}>
       <Header />
       <BannerContainer>
-        <Banner delay={0}>
-          <TextContainer delay={0}>
-            <h1>Title for banner 1</h1>
-          </TextContainer>
-          <ImageContainer>
-            <StyledImage src="/art-display/1.jpeg" />
-          </ImageContainer>
-        </Banner>
-        <Banner delay={6}>
-          <TextContainer delay={6}>
-            <h1>Title for banner 2</h1>
-          </TextContainer>
-          <ImageContainer>
-            <StyledImage src="/art-display/2.jpeg" />
-          </ImageContainer>
-        </Banner>
+        <div style={{maxWidth: '1500px'}}>
+          <Banner delay={0}>
+            <TextContainer delay={0}>
+              <h1>Title for banner 1</h1>
+            </TextContainer>
+            <ImageContainer>
+              <StyledImage src="/art-display/1.jpeg" />
+            </ImageContainer>
+          </Banner>
+          <Banner delay={12}>
+            <TextContainer delay={12}>
+              <h1>Title for banner 2</h1>
+            </TextContainer>
+            <ImageContainer>
+              <StyledImage src="/art-display/2.jpeg" />
+            </ImageContainer>
+          </Banner>
+        </div>
       </BannerContainer>
     </main>
   );
