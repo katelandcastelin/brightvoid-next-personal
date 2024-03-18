f2abdceaffaaac23dcdefd80cdb8b7562071e5fa
katelandcastelin <kateland.cas@gmail.com>
2024-03-18 12:58:00 +0200

    feat/product-details-styling: adding responsive design

--- app/components/ProductDetails.jsx
+++ app/components/ProductDetails.jsx
@@ -10,22 +10,95 @@ const Container = styled.div`
   max-width: 1500px;
   margin-top: 100px;
   display: flex;
+  padding: 20px;
+`;
+
+const SecondContainer = styled.div`
+  height: 100vh;
+  margin-top: 5%;
+  display: flex;
+  justify-content: center;
+  padding: 0 3rem;
+
+  @media (max-width: 1040px) {
+    flex-direction: column;
+    padding: 3rem;
+    height: 100%;
+  }
+
+  a {
+    text-decoration: underline;
+
+    :hover {
+      color: #5b5b5b;
+    }
+
+    :active {
+      color: #000;
+    }
+  }
 `;
 
 const ImageAndThumbnailContainer = styled.div`
+  /* display: flex;
+  max-width: 600px;
+  width: 100%; */
   display: flex;
+  flex-direction: row-reverse;
+  justify-content: center;
+
+  @media (max-width: 1040px) {
+    display: flex;
+    margin-bottom: 40px;
+  }
+
+  @media (max-width: 780px) {
+    display: contents;
+  }
 `;
 
 const ImageContainer = styled.div`
+  /* margin-left: 20px;
   height: 70vh;
   width: 600px;
   display: flex;
   justify-content: center;
   align-items: center;
-  overflow: hidden;
+  overflow: hidden; */
+  display: flex;
+  justify-content: center;
+  height: 90%;
+  width: 100%;
+  width: 700px;
+  margin: 0 10px;
+
+  @media (max-width: 1290px) {
+    width: 650px;
+  }
+
+  @media (max-width: 1190px) {
+    width: 580px;
+  }
+
+  @media (max-width: 1140px) {
+    width: 530px;
+  }
+
+  @media (max-width: 1095px) {
+    width: 500px;
+  }
+
+  @media (max-width: 1040px) {
+    width: 100%;
+    height: 90vh;
+  }
+
+  @media (max-width: 780px) {
+    height: 60vh;
+    margin: 0 0 10px 0;
+  }
 
   img {
-    height: 100%;
     width: 100%;
     object-fit: cover;
   }
@@ -37,15 +110,17 @@ export default function ProductDetails({ product }) {
 
   return (
     <div style={{display: 'flex', justifyContent: 'center'}}>
-    <Container>
-      <ImageAndThumbnailContainer>
-        <ThumbnailGallery images={productImages} selectedImage={selectedImage} setSelectedImage={setSelectedImage} />
-        <ImageContainer>
-          <img src={selectedImage} />
-        </ImageContainer>
-      </ImageAndThumbnailContainer>
-      <div style={{ flex: 1 }}>TEST</div>
-    </Container>
+      <Container>
+        <SecondContainer>
+        <ImageAndThumbnailContainer>
+          <ThumbnailGallery images={productImages} selectedImage={selectedImage} setSelectedImage={setSelectedImage} />
+          <ImageContainer>
+            <img src={selectedImage} />
+          </ImageContainer>
+        </ImageAndThumbnailContainer>
+        <div style={{ flex: 1 }}>TEST</div>
+        </SecondContainer>
+      </Container>
     </div>
   )
 }
--- app/components/ThumbnailGallery.jsx
+++ app/components/ThumbnailGallery.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Container = styled.div`
-  height: 70vh;
-  overflow-y: auto;
+const VerticalThumbnailList = styled.div`
+  height: 90%;
 
   @media (max-width: 1040px) {
     height: 90vh;
@@ -12,12 +11,23 @@ const Container = styled.div`
   @media (max-width: 780px) {
     display: none;
   }
+  /* height: 70vh;
+  overflow-y: auto;
+
+  @media (min-width: 920px) {
+    flex-direction: row;
+  } */
 `;
 
 const ThumbnailList = styled.div`
   max-height: 100%;
   height: auto;
-  width: 150px;
+  max-width: 150px;
+  width: 100%;
+
+  /* @media (max-width: 920px) {
+    display: none;
+  } */
 `;
 
 const ImageThumbnail = styled.div`
@@ -31,17 +41,19 @@ const ImageThumbnail = styled.div`
 `;
 
 const HorizontalThumbnailList = styled.div`
-  display: none;
+  /* display: none;
 
-  @media (max-width: 780px) {
+  @media (max-width: 920px) {
     display: flex;
-    overflow-y: scroll;
-  }
+    overflow-x: auto;
+    margin-top: auto;
+    margin-bottom: auto;
+    width: 100vw;
+  } */
 `;
 
 const HorizontalImageThumbnail = styled.div`
   display: none;
-
   @media (max-width: 780px) {
     display: flex;
     cursor: pointer;
@@ -53,12 +65,25 @@ const HorizontalImageThumbnail = styled.div`
       margin: 0 3px;
     }
   }
+  /* display: none;
+
+  @media (max-width: 920px) {
+    display: flex;
+    cursor: pointer;
+
+    img {
+      height: 150px;
+      width: 150px;
+      object-fit: cover;
+      margin: 0 3px;
+    }
+  } */
 `;
 
 export default function ThumbnailGallery({ images, setSelectedImage }) {
   return (
     <div>
-      <Container>
+      <VerticalThumbnailList>
         <ThumbnailList cols={1} rowHeight={164}>
           {images.map((image, index) => (
             <ImageThumbnail key={index} onClick={() => setSelectedImage(image)}>
@@ -71,7 +96,7 @@ export default function ThumbnailGallery({ images, setSelectedImage }) {
             </ImageThumbnail>
           ))}
         </ThumbnailList>
-      </Container>
+      </VerticalThumbnailList>
 
       <HorizontalThumbnailList>
         {images.map((image, index) => (
