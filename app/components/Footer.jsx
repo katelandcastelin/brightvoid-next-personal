8101e3710e03396edcf8fcaafa1f5395ae4ac475
katelandcastelin <kateland.cas@gmail.com>
2024-04-01 23:24:41 +0200

    feat/footer-styling: added animated background

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,6 +1,6 @@
 'use client';
 import React from 'react';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
 const Container = styled.div`
   height: 350px;
@@ -8,33 +8,36 @@ const Container = styled.div`
   border: 1px solid #282828;
   margin: 40px 10px 10px 10px;
   padding: 10px;
-  background: radial-gradient(circle at center bottom, rgba(59, 59, 59, 0.8) 0%, #0d0d0d 70%);
+  background: radial-gradient(circle at center bottom, rgba(86, 86, 86, 0.8) 0%, #000 70%);
   background-size: 100% 200%;
-
-  p {
-    font-family: monospace;
-    font-size: large;
-    text-transform: uppercase;
-    color: #d7ffc7;
-  }
+  position: relative;
+  overflow: hidden;
 `;
 
 const Content = styled.div`
-  display: flex;
+  text-align: center;
+
+  a {
+    gap: 10px;
+    font-family: monospace;
+    font-size: large;
+    color: #f0ffea;
+    transition: text-shadow 0.3s ease-in-out;
+
+    &:hover {
+      text-shadow: 0 0 10px #dcffcf, 0 0 3px #dcffcf, 0 0 1px #dcffcf;
+    }
+  }
 `;
 
 const SocialsLinks = styled.div`
   display: flex;
   justify-content: center;
-  /* gap: 10px; */
-
-  a {
-    margin-right: 10px;
-  }
+  gap: 10px;
   
   img {
-    height: 50px;
-    border-radius: 3px;
+    height: 45px;
+    border-radius: 4px;
     padding: 7px;
 
     &:hover {
@@ -43,11 +46,41 @@ const SocialsLinks = styled.div`
   }
 `;
 
+const glitch = keyframes`
+  to {
+    clip-path: polygon(0 calc(100% - 0.5em), 100% calc(100% - 0.5em), 100% 100%, 0 100%);
+  }
+`;
+
+const Overlay = styled.div`
+  display: block;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: transparent;
+  background-size: 5px 5px, 5px 5px;
+  background-position: -1px -1px, -1px -1px;
+  background-image:
+    linear-gradient(rgba(255, 255, 255, 0.05) 1px, transparent 1px),
+    linear-gradient(90deg, rgba(255, 255, 255, 0.05) 1px, transparent 1px);
+  mix-blend-mode: difference;
+`;
+
+const GlitchOverlay = styled(Overlay)`
+  clip-path: polygon(0 0, 100% 0, 100% 0.5em, 0 0.5em);
+  animation: ${glitch} 10s linear infinite;
+  transform: translateX(0.1rem);
+`;
+
 export default function Footer () {
   return (
     <Container>
+      <Overlay />
+      <GlitchOverlay />
       <div>
-        <div style={{textAlign: 'center', marginBottom: '10px'}}>
+        <div style={{textAlign: 'center', marginBottom: '10px', textTransform: 'uppercase'}}>
           <p>Get in touch</p>
         </div>
         <SocialsLinks>
@@ -72,11 +105,22 @@ export default function Footer () {
         </SocialsLinks>
       </div>
       <Content>
-          <div>
-            HELLO THERE
-            <p>Some content</p>
-          </div>
-        </Content>
+        <a href='/pages/contact'>
+          Contact
+        </a>
+        <a href='/pages/submitArt' style={{margin: '0 20px'}}>
+          Submit your art
+        </a>
+        <a href='/pages/gallery'>
+          Our artists
+        </a>
+      </Content>
+      <div>
+        HELLO THERE
+        <br />
+        <br />
+        more content
+      </div>
     </Container>
   )
 }
